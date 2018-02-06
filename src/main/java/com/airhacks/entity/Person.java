57edab03f2c058b243969fc9eb0b1cd194ca20cf
rmortale <rmortale@gmx.net>
2018-02-06 19:26:36 +0100

    added entity

---
+++ src/main/java/com/airhacks/entity/Person.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.airhacks.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Nino
+ */
+@Entity
+@Table(name = "T_PERSON")
+public class Person implements Serializable {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Id
+    @GeneratedValue()
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "FIRST_NAME")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "LAST_NAME")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
